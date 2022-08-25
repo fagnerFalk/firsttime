ced4be95df30bf61345329fa2193b9795c29cc8e
fagnerFalk <fagner.falk02@gmail.com>
2022-08-25 16:59:18 -0300

    upload pratica-1

--- tarefa-2/pratica-1.js
+++ tarefa-2/pratica-1.js
@@ -3,8 +3,15 @@ let email
 let profissao
 let idade
 let pais
-nome={Fagner Falk}
-email={fagner.falk02@gmail.com}
-profissao={DevFrontEnd}
-pais={Belize}
+nome="Fagner Falk"
+email="fagner.falk02@gmail.com"
+profissao="DevFrontEnd"
+idade="37 anos"
+pais="Belize"
 // até aqui tudo bem Agora precisamos dar o comando console.logpara exibir no terminal
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(pais)
+ 
