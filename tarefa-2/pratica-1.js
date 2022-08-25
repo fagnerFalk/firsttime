edab365b0b17eac42aebaabbcdce13be01964bf7
fagnerFalk <fagner.falk02@gmail.com>
2022-08-25 16:42:17 -0300

    uploadTarefa-2-pt-2

---
+++ tarefa-2/pratica-1.js
@@ -0,0 +1,10 @@
+let nome
+let email
+let profissao
+let idade
+let pais
+nome={Fagner Falk}
+email={fagner.falk02@gmail.com}
+profissao={DevFrontEnd}
+pais={Belize}
+// até aqui tudo bem Agora precisamos dar o comando console.logpara exibir no terminal
