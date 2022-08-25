5ce3a1de3543d662a7fb37f4c7a058a01b4f0886
fagnerFalk <fagner.falk02@gmail.com>
2022-08-25 17:31:03 -0300

    uploadTarefaPronta

--- tarefa-2/pratica-1.js
+++ tarefa-2/pratica-1.js
@@ -14,4 +14,8 @@ console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(pais)
- 
+
+let mensagem
+mensagem=`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${pais}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email} ? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+
+console.log(mensagem)
