01a2fbfec92831ff9f2b0eaef5bae8ceeb86fae6
fagnerFalk <fagner.falk02@gmail.com>
2022-08-27 14:57:53 -0300

    uploadTarefa-3Pronta

--- tarefa-2/pratica-3.js
+++ tarefa-2/pratica-3.js
@@ -7,3 +7,4 @@ if (mensagem > 6110) {
 }  else {
     console.log ("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D")
 }
+
